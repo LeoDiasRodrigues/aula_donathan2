e99632cdc49dfe24b67db676288f6cc6a3ae37d9
LeoDiasRodrigues <leozezudr@gmail.com>
2026-04-01 17:11:42 -0300

    Add files via upload

---
+++ aula_donathan2/router.js
@@ -0,0 +1,10 @@
+
+const bodyParser = require('body-parser');
+const person = require('./routes/person');
+ 
+module.exports = (app) => {
+    app.use(
+        bodyParser.json(),
+		person
+    )
+}
